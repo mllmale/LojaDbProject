060d8ef9c225905452520e1bfd14411b0911c0d7
Maria Letícia A. Gonçalves <105677223+mllmale@users.noreply.github.com>
2024-08-12 14:20:24 -0300

    Update Loja.java

--- LojaDbProject/src/loja/Loja.java
+++ LojaDbProject/src/loja/Loja.java
@@ -1,19 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package loja;
 import loja.style.Login;
 
-/**
- *
- * @author leticia
- */
 public class Loja {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Login tela = new Login();
         tela.setVisible(true);
