31632ba8f9296629c364d822f3629febc69ea59b
Maria Letícia A. Gonçalves <105677223+mllmale@users.noreply.github.com>
2024-08-12 14:20:51 -0300

    Update ModuloConexao.java

--- LojaDbProject/src/dal/ModuloConexao.java
+++ LojaDbProject/src/dal/ModuloConexao.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dal;
 
 import java.sql.*;
 
-/**
- *
- * @author leticia
- */
 public class ModuloConexao {
     // Método responsável por estabelecer a conexão com o banco
     
